852ef5961609ae46f11e1afce38f329f4c3f5919
xutianyish <xutianyish@gmail.com>
2019-07-11 22:23:38 -0400

    Update data_loader.py

--- VAE/data_loader.py
+++ VAE/data_loader.py
@@ -19,29 +19,3 @@ def get_data_loader(batch_size=64):
   dataset = torchvision.datasets.ImageFolder(root='../../face_data', transform=transform)
   train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=1, shuffle=True)
   return train_loader
-
-'''
-if __name__ == '__main__':
-    # it = DataLoader([1,2,3,45,5], batch_size=2, num_workers=1)
-    # print('started')
-    # for i in it:
-    #     print(i)
-    train_loader = get_data_loader(batch_size=4)
-    print(len(train_loader))
-    k = 0
-    for images, label in train_loader:
-        print(images.shape)
-        image = images[0]
-        # place the colour channel at the end, instead of at the beginning
-        img = np.transpose(image, [1,2,0])
-        # normalize pixel intensity values to [0, 1]
-        img = img / 2 + 0.5
-        plt.subplot(3, 5, k+1)
-        plt.axis('off')
-        plt.imshow(img)
-        k += 1
-        if k > 14:
-           break
-
-    plt.show()
-'''
